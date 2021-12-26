8fa3185d1b98eef9358d7e20e5c436aaac27a1e0
eminentpal <eziechukwudi@gmail.com>
2021-12-26 22:44:46 +0100

    added back long url again

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -172,13 +172,13 @@ const Home = (props) => {
           {urlItems.map((item) => (
             <div key={item.linkId} className="linkCont">
               <div className="linkDiv">
-               {/* <p style={{ marginBottom: "15px" }} href={`/${item.url}`}>
+               <p style={{ marginBottom: "15px" }} href={`/${item.url}`}>
                   {item?.url?.length > 27 ? (
                     <span>{item.url?.substring(0, 25)}...</span>
                   ) : (
                     <span>{item.url}</span>
                   )}
-                </p> */}
+                </p>
                 <a href={`/${item.linkId}`}>shutly.vercel.app/{item.linkId}</a>
                 <CopyToClipboard
                   options={{ debug: props.debug, message: "" }}
